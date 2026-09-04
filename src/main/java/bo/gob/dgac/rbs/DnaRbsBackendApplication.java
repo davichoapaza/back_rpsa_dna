f91e899ac3882cfd027e1c775e7d771dba9e471b
David APAZA <dapaza@dgac.gob.bo>
2026-09-04 16:38:36 -0400

    primer file

---
+++ src/main/java/bo/gob/dgac/rbs/DnaRbsBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.gob.dgac.rbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaRbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaRbsBackendApplication.class, args);
+	}
+
+}
